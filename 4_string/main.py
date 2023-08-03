40ff6a8b8c8d42c5871022c36e217cac56810f9b
Naufal Muafi <nmuafi1@gmail.com>
2023-08-03 18:14:30 +0700

    add: exercise 4 is done.

---
+++ 4_string/main.py
@@ -0,0 +1,22 @@
+# PROBLEM 1
+street, city, country = "plemburan st.", "sleman", "indonesia"
+address = f"{street}, {city}, {country}"
+print(address)
+
+
+# PROBLEM 2
+text = "Earth revolves around the sun"
+x = slice(6, 14)
+print(text[x])
+print(text[-3:])
+
+
+# PROBLEM 3
+fruits, vegetables = 5, 3
+print(f"I eat {vegetables} veggies and {fruits} fruits daily")
+
+
+# PROBLEM 4
+s = "maine 200 banana khaye"
+s = s.replace('200', '10').replace('banana', 'samosa')
+print(s)
