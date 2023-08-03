62d15a7eda8f1b5834b3a6d4a16a78a2e3632441
Naufal Muafi <nmuafi1@gmail.com>
2023-08-03 18:00:33 +0700

    add: exercise 3 is done.

---
+++ 3_numbers/main.py
@@ -0,0 +1,25 @@
+# PROBLEM 1
+long, wide = 92, 48.8
+area = long * wide
+
+print(f"Total Area = {area}")
+
+
+# PROBLEM 2
+packets, cost, give_out = 9, 1.49, 20
+
+give_in = give_out - (packets * cost)
+print(f"The Shopkeeper will give you back ${give_in}")
+
+
+# PROBLEM 3
+length, price = 5.5, 500
+area = length**2
+cost = area * price
+
+print(f"cost per square feet is ${cost}")
+
+
+# PROBLEM 4
+print(bin(17))
+print(format(17, 'b'))
